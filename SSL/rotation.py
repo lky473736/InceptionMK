09cf7c000406b734feb7fefe89f5502b69be3d04
Gyuyeon Lim <lky473736@icloud.com>
2025-10-12 16:26:36 +0900

    cf

--- SSL/rotation.py
+++ SSL/rotation.py
@@ -1,47 +1,51 @@
-# train/rotation.py
+# SSL/rotation.py
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
-import torchvision.transforms as transforms
 import argparse
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from model import YourBackboneModel
+from model import InceptionMK
 
 class RotationDataset(torch.utils.data.Dataset):
-    def __init__(self, base_dataset, transform=None):
+    def __init__(self, base_dataset):
         self.base_dataset = base_dataset
-        self.transform = transform
         
     def __len__(self):
         return len(self.base_dataset)
     
     def __getitem__(self, idx):
-        img, _ = self.base_dataset[idx]
+        data, _ = self.base_dataset[idx]
         rotation_label = torch.randint(0, 4, (1,)).item()
-        angle = rotation_label * 90
-        img_rotated = transforms.functional.rotate(img, angle)
-        if self.transform:
-            img_rotated = self.transform(img_rotated)
-        return img_rotated, rotation_label
+        
+        if rotation_label == 1:
+            data = torch.roll(data, shifts=data.shape[0]//4, dims=0)
+        elif rotation_label == 2:
+            data = torch.roll(data, shifts=data.shape[0]//2, dims=0)
+        elif rotation_label == 3:
+            data = torch.roll(data, shifts=3*data.shape[0]//4, dims=0)
+        
+        return data, rotation_label
 
 def pretrain(backbone, train_loader, args):
-    model = nn.Sequential(backbone, nn.Linear(args.feature_dim, 4)).to(args.device)
+    classifier = nn.Linear(args.feature_dim, 4).to(args.device)
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=args.pretrain_lr, weight_decay=args.weight_decay)
+    optimizer = optim.Adam(list(backbone.parameters()) + list(classifier.parameters()), 
+                          lr=args.pretrain_lr, weight_decay=args.weight_decay)
     
     for epoch in range(args.pretrain_epochs):
-        model.train()
+        backbone.train()
+        classifier.train()
         total_loss = 0
         correct = 0
         total = 0
         
-        for images, labels in train_loader:
-            images, labels = images.to(args.device), labels.to(args.device)
+        for data, labels in train_loader:
+            data, labels = data.to(args.device), labels.to(args.device)
             optimizer.zero_grad()
-            outputs = model(images)
+            features = backbone.forward_features(data)
+            outputs = classifier(features)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
@@ -69,10 +73,10 @@ def downstream(backbone, train_loader, val_loader, args, num_classes):
         correct = 0
         total = 0
         
-        for images, labels in train_loader:
-            images, labels = images.to(args.device), labels.to(args.device)
+        for data, labels in train_loader:
+            data, labels = data.to(args.device), labels.to(args.device)
             with torch.no_grad():
-                features = backbone(images)
+                features = backbone.forward_features(data)
             optimizer.zero_grad()
             outputs = classifier(features)
             loss = criterion(outputs, labels)
@@ -99,10 +103,10 @@ def downstream(backbone, train_loader, val_loader, args, num_classes):
         correct = 0
         total = 0
         
-        for images, labels in train_loader:
-            images, labels = images.to(args.device), labels.to(args.device)
+        for data, labels in train_loader:
+            data, labels = data.to(args.device), labels.to(args.device)
             optimizer.zero_grad()
-            features = backbone(images)
+            features = backbone.forward_features(data)
             outputs = classifier(features)
             loss = criterion(outputs, labels)
             loss.backward()
@@ -122,9 +126,9 @@ def evaluate(backbone, classifier, val_loader, args):
     total = 0
     
     with torch.no_grad():
-        for images, labels in val_loader:
-            images, labels = images.to(args.device), labels.to(args.device)
-            features = backbone(images)
+        for data, labels in val_loader:
+            data, labels = data.to(args.device), labels.to(args.device)
+            features = backbone.forward_features(data)
             outputs = classifier(features)
             _, predicted = outputs.max(1)
             total += labels.size(0)
@@ -139,11 +143,12 @@ if __name__ == '__main__':
     parser.add_argument('--pretrain_lr', type=float, default=0.001)
     parser.add_argument('--batch_size', type=int, default=128)
     parser.add_argument('--weight_decay', type=float, default=1e-4)
-    parser.add_argument('--feature_dim', type=int, default=512)
+    parser.add_argument('--input_channels', type=int, default=9)
+    parser.add_argument('--feature_dim', type=int, default=128)
     parser.add_argument('--num_classes', type=int, default=10)
     parser.add_argument('--device', type=str, default='cuda')
     args = parser.parse_args()
     
-    backbone = YourBackboneModel().to(args.device)
+    backbone = InceptionMK(input_channels=args.input_channels, embedding_dim=args.feature_dim).to(args.device)
     pretrain(backbone, pretrain_loader, args)
     downstream(backbone, train_loader, val_loader, args, args.num_classes)
